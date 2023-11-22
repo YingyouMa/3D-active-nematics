bb5933c89520db29cecfe939bfa7274e4f1e96bc
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2023-11-21 21:05:56 -0500

    Reorganize

--- loop-analysis/show_loop_plane.py
+++ loop-analysis/show_loop_plane.py
@@ -1,19 +1,61 @@
 import numpy as np
 
+def main(loop_box_coord, S_whole, n_whole, width, margin_ratio=0.6):
 
-# Derive and take the average of the local Q tensor with the director field around the loop
-Q = np.einsum('iabc, jabc -> ijabc', n_box, n_box)
-Q = np.average(Q, axis=(-1,-2, -3))
-Q = Q - np.diag((1,1,1))/3
+	N = np.shape(S)[0] 	# grid number on each dimension
+	dis = 1/N*width 	# grid size
 
-# Diagonalisation and sort the eigenvalues.
-eigval, eigvec = np.linalg.eig(Q)
-eigvec = np.transpose(eigvec)
-eigidx = np.argsort(eigval)
-eigval = eigval[eigidx]
-eigvec = eigvec[eigidx]
+	dmean = setup(loop_box_coord, S_whole, n_whole, width, margin_ratio=margin_ratio)
 
-# The directors within one cross section of the loop will be shown
-# Select the cross section by its norm vector
-# The norm of the principle plane is the eigenvector corresponding to the smallest eigenvalue
-norm_vec = eigvec[0]
+
+def setup(loop_box_coord, S_whole, n_whole, width, margin_ratio=0.6):
+
+	# Find the region enclosing the loop. The size of the region is controlled by margin_ratio
+	xrange, yrange, zrange = loop_box[:,1] - loop_box[:,0]
+	margin = ( np.array([xrange, yrange, zrange]) * margin_ratio/2 ).astype(int)
+	loop_box[:,0] -= margin
+	loop_box[:,1] += margin
+
+	# Consider the periodical boundary condition (index out of the simulation box)
+	xmin, ymin, zmin = loop_box[:,0]
+	xmax, ymax, zmax = loop_box[:,1]
+	N = np.shape(S)[0]
+	sl0 = np.array(range(xmin, xmax+1)).reshape(-1,1, 1)%N
+	sl1 = np.array(range(ymin, ymax+1)).reshape(1,-1, 1)%N
+	sl2 = np.array(range(zmin, zmax+1)).reshape(1,1,-1)%N
+
+	# Select the local n and S around the loop
+	n_box = n_whole[:, sl0,sl1,sl2]
+	S_box = S_whole[sl0,sl1,sl2]
+
+	# Derive and take the average of the local Q tensor with the director field around the loop
+	Q = np.einsum('iabc, jabc -> ijabc', n_box, n_box)
+	Q = np.average(Q, axis=(-1,-2, -3))
+	Q = Q - np.diag((1,1,1))/3
+
+	# Diagonalisation and sort the eigenvalues.
+	eigval, eigvec = np.linalg.eig(Q)
+	eigvec = np.transpose(eigvec)
+	eigidx = np.argsort(eigval)
+	eigval = eigval[eigidx]
+	eigvec = eigvec[eigidx]
+
+	# The directors within one cross section of the loop will be shown
+	# Select the cross section by its norm vector
+	# The norm of the principle plane is the eigenvector corresponding to the smallest eigenvalue
+	norm_vec = eigvec[0]
+
+	# Build the grid for visualization
+	x = np.arange( loop_box[0][0], loop_box[0][-1]+1)/N*width
+	y = np.arange( loop_box[1][0], loop_box[1][-1]+1)/N*width
+	z = np.arange( loop_box[2][0], loop_box[2][-1]+1)/N*width
+	X, Y, Z = np.meshgrid(x,y,z, indexing='ij')
+
+	# Find the height of the middle cross section: dmean
+	r_box = np.array((X, Y, Z))
+	d_box = np.einsum('iabc, i -> abc', r_box, norm_vec)
+	dmean = np.average(d_box)
+
+	return dmean
+
+def make_plot(upper, down, ):
---
+++ nematic3D/field.py
@@ -0,0 +1,62 @@
+# ------------------------------------
+# Analysis of Q field in 3D
+# Yingyou Ma, Physics @ Brandeis, 2023
+# ------------------------------------
+
+import numpy as np
+
+def diagonalize(qtensor):
+  """
+  Diagonalization of Q tensor in 3D nematics.
+  Currently it onply provides the uniaxial information.
+  Will be updated to derive biaxial analysis in the future.
+  Algorythm provided by Matthew Peterson: 
+
+  Parameters
+  ----------
+  qtensor:  numpy array, tensor order parameter Q of each grid
+            shape: (N, M, L, 5), where N, M and L are the number of grids in each dimension.
+            qtensor[..., 0] = Q_xx, qtensor[..., 1] = Q_xy, and so on. 
+
+  Returns
+  -------
+  S:  numpy array, the biggest eigenvalue as the scalar order parameter of each grid
+      shape: (N, M, L)
+  n:  numpy array, the eigenvector corresponding to the biggest eigenvalue, as the director, of each grid.
+      shape: (N, M, L)
+  """
+  
+  # derive Q field and calculate it with np.einsum() and np.linalg.det()
+  N, M, L = np.shape(Q)[:3]
+  Q = np.zeros( (N, M, L, 3, 3)  )
+  Q[..., 0,0] = qtensor[0]
+  Q[..., 0,1] = qtensor[1]
+  Q[..., 0,2] = qtensor[2]
+  Q[..., 1,0] = qtensor[1]
+  Q[..., 1,1] = qtensor[3]
+  Q[..., 1,2] = qtensor[4]
+  Q[..., 2,0] = qtensor[2]
+  Q[..., 2,1] = qtensor[4]
+  Q[..., 2,2] = - Q[..., 0,0] - Q[..., 1,1]
+
+  p = 0.5 * np.einsum('ijkab, ijkba -> ijk', Q, Q)
+  q = np.linalg.det(Q)
+  r = 2 * np.sqrt( p / 3 )
+  del Q
+
+  # derive S and n
+  S = r * np.cos( 1/3 * np.arccos( 4 * q / r**3 ) )
+  temp = np.array( [
+      qtensor[2] * ( qtensor[3] - S ) - qtensor[1] * qtensor[4] ,
+      qtensor[4] * ( qtensor[0] - S ) - qtensor[1] * qtensor[2] ,
+      qtensor[1]**2 - ( qtensor[0] - S ) * ( qtensor[3] - S  )
+      ] )
+  n = temp / np.linalg.norm(temp, axis = 0)
+
+  return S, n
+
+
+    
+  
+  
+  
