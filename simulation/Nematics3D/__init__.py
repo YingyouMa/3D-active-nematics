c9e6d4fd41819aba8be019c0a92d016d2954fd3c
Yingyou Ma <65419573+YingyouMa@users.noreply.github.com>
2024-09-05 22:55:28 -0400

    Update __init__.py

--- simulation/Nematics3D/__init__.py
+++ simulation/Nematics3D/__init__.py
@@ -1,5 +1,6 @@
 from .field import *
 from .disclination import *
-from .classes.smoothened_line import Class_smoothened_line
+from .classes.smoothened_line import SmoothenedLine
+from .classes.disclination_line import DisclinationLine
 
 __version__ = '0.0.0'
